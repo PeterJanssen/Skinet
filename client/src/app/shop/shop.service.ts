a7b0d1b3b31a8bcd9e98c8a37bdef58f6bf4d389
Peter Janssen <32429237+PeterJanssen@users.noreply.github.com>
2021-03-11 22:03:43 +0100

    End of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/ProductType';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -47,6 +48,10 @@ export class ShopService {
 
   getBrands() {
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getTypes() {
