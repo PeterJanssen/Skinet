ca174ca4d1fd3facf0946cc8eaf91d35b39ee6a8
Peter Janssen <32429237+PeterJanssen@users.noreply.github.com>
2021-08-29 21:57:52 +0200

    Remove unnused field and add validation to BaseSpecParams tp prevent negative numbers

--- Application/Core/Specifications/BaseSpecParams.cs
+++ Application/Core/Specifications/BaseSpecParams.cs
@@ -1,17 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Application.Core.Specifications
 {
     public class BaseSpecParams
     {
+        [Range(1, int.MaxValue)]
         protected int MaxPageSize { get; set; } = 50;
+        [Range(1, int.MaxValue)]
         public int PageIndex { get; set; } = 1;
         protected int _pageSize = 6;
+
+        [Range(1, int.MaxValue)]
         public int PageSize
         {
             get => _pageSize;
             set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
         }
         public string Sort { get; set; }
-
         protected string _search;
         public string Search { get => _search; set => _search = value.ToLower(); }
     }
--- Application/Core/Specifications/OrderSpec/OrderSpecParams.cs
+++ Application/Core/Specifications/OrderSpec/OrderSpecParams.cs
@@ -2,7 +2,6 @@ namespace Application.Core.Specifications.OrderSpec
 {
     public class OrderSpecParams : BaseSpecParams
     {
-        private new int _pageSize = 10;
         public int Status { get; set; }
     }
 }
